621f1b7e44b492cb23ca75fb9ff8776cea9d2b01
IS <ivoseeba@gmail.com>
2021-09-16 06:31:06 +0300

    -

--- index.js
+++ index.js
@@ -108,4 +108,6 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+
+docker rm -vf $(docker ps -a -q)
 */
